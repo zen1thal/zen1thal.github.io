49aa22166d80b91b45d7ff12a6632b3c53fb40c2
Sviatoslav Syniuk <sviatoslav.syniuk2003@gmail.com>
2023-11-19 14:31:45 +0100

    Sass fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,35 +15,34 @@ function hide_menu() {
   nav.style.right = "-60vw";
   background_menu.style.display = "none";
 }
-
 function switchMode() {
-  const body = document.querySelector("body");
+  const view = document.querySelector(".view");
   const header1 = document.querySelector("h1");
   const header2 = document.querySelector("h2");
   const footer = document.querySelector(".page-footer");
   const pageHeader = document.querySelector(".page-header");
   const hobbies = document.querySelector(".card-cont__hobbies");
-  const cardContent = document.querySelector(".static-container__card-cont");
+  const cardContent = document.querySelector(".card-cont");
 
-  if (body.classList.contains("light-mode")) {
-    body.classList.remove("light-mode");
-    body.style.backgroundColor = "var(--gray)";
-    body.style.color = "white";
+  if (view.classList.contains("light-mode")) {
+    view.classList.remove("light-mode");
+    view.style.backgroundColor = "#1c1e1f";
+    view.style.color = "white";
     header1.style.color = "white";
     header2.style.color = "white";
-    hobbies.style.color = "var(--slate)";
-    footer.style.backgroundColor = "var(--black)";
-    cardContent.style.backgroundColor = "var(--black)";
-    pageHeader.style.backgroundColor = "var(--black)";
+    hobbies.style.color = "--slate";
+    footer.style.backgroundColor = "#15181b";
+    cardContent.style.backgroundColor = "#15181b";
+    pageHeader.style.backgroundColor = "#15181b";
   } else {
-    body.classList.add("light-mode");
-    body.style.backgroundColor = "white";
-    body.style.color = "var(--light-teal)";
-    header1.style.color = "var(--light-gray)";
-    header2.style.color = "var(--light-gray)";
+    view.classList.add("light-mode");
+    view.style.backgroundColor = "white";
+    view.style.color = "#ccd6f6";
+    header1.style.color = "#929292";
+    header2.style.color = "#929292";
     hobbies.style.color = "white";
-    footer.style.backgroundColor = "var(--light-gray)";
-    cardContent.style.backgroundColor = "var(--light-gray)";
-    pageHeader.style.backgroundColor = "var(--light-gray)";
+    footer.style.backgroundColor = "#929292";
+    cardContent.style.backgroundColor = "#929292";
+    pageHeader.style.backgroundColor = "#929292";
   }
 }
