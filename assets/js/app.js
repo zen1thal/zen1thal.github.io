b9ce4f38cfdb7e9592d77b2bfa2e4ea1784f7aa8
zviatos <sviatoslav.syniuk2003@gmail.com>
2024-02-18 15:16:16 +0100

    Added scroll animations, custom cursor and projects section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+  var $cursor = $(".cs");
+  $(window).on("mousemove", function (e) {
+    $cursor.css({
+      left: e.clientX - $cursor.width() / 2,
+      top: e.clientY - $cursor.height() / 2,
+    });
+  });
+  $("a, .page-header__logo, .right-side")
+    .on("mouseenter", function () {
+      $cursor.css("transform", "rotateZ(35deg)");
+      $(".cs :nth-child(2)").css("opacity", "1");
+    })
+    .on("mouseleave", function () {
+      $(".cs :nth-child(2)").css("opacity", "0");
+      $cursor.css("transform", "rotateZ(0deg)");
+    });
+
+
+  $(document).on("click", 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      500
+    );
+  });
   $("#page-header__hamburger").click(function () {
     $("nav").css("right", "0");
     $("#back_menu").css("display", "block");
